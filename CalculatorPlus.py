64aa7c2a49475ec1b14a143562f8f2a8eb6f5bb6
tilak pechetti <tilak.pechetti@gmail.com>
2023-08-02 19:38:31 +0530

    added sqrt to Feature/sqrt branch

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -1,25 +1,33 @@
-import math class Calculator:
+import math 
+class Calculator:
 
-def add(self, a, b): return a + b
-def subtract(self, a, b): return a - b
-def multiply(self, a, b):
+    def add(self, a, b): 
+        return a + b
+    def subtract(self, a, b): 
+        return a - b
+    def multiply(self, a, b):
+        return a * b
 
-return a * b
-
-def divide(self, a, b): return a / b
+    def divide(self, a, b): 
+        return a / b
 
 # TODO: Implement the following function to calculate the square root of a number.
 
-# def square_root(self, x):
+def square_root(self, x):
 
-#     return math.sqrt(x)
+     return math.sqrt(x)
 
 # You need to uncomment the above function and complete its implementation to add the square root feature.
 
-if      name      == "  main       ": calculator = Calculator()
+if  "__name__" == "__main__": calculator = Calculator()
 
 num1 = 16
 
 num2 = 4
+num3 = 25
 
-print(f"{num1} + {num2} = {calculator.add(num1, num2)}") print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}") print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+print(f"{num1} + {num2} = {calculator.add(num1, num2)}") 
+print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
+print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}") 
+print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+print(f"The square root of {num3} = {calculator.square_root(num3)}")
