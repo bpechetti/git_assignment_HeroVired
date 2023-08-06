79eb966bd859024efcf50fe8ac3654bb96a4ff8c
tilak pechetti <tilak.pechetti@gmail.com>
2023-08-06 20:10:36 +0530

    Implement rectangle area feature

--- geometry-calculator.py
+++ geometry-calculator.py
@@ -5,16 +5,16 @@ class GeometryCalculator:
         return math.pi * radius ** 2
     def calculate_rectangle_area(self, length, width): 
         return length * width
-if "__name__" == "__main__": 
+if __name__ == "__main__": 
     calculator = GeometryCalculator()
 # TODO: Implement the feature to calculate the area of a circle # 
-radius = 5
+    radius = 5
 
-print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
+    print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
 
 # TODO: Implement the feature to calculate the area of a rectangle # 
 
-length = 10
-width = 6
+    length = 10
+    width = 6
 
-print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
+    print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
